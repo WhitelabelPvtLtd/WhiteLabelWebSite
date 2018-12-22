16314f2be47dc02f78493e38936cec56e3e283b7
paysysGlobal <46065624+paysysGlobal@users.noreply.github.com>
2018-12-22 14:56:18 +0500

    First Upload of white label ATM web site

---
+++ Email.aspx.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Model_Email : System.Web.UI.Page
+{
+    EmailService es = new EmailService();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    [WebMethod]
+    public static string callEmail(string sub, string body)
+    {
+        EmailService es = new EmailService();
+        var result = es.SendEmail(sub, body);
+        return result;
+    }
+}
